cb495f1b5ae47277dceb00e2589fc558e0ee7488
Kevin <kev.saintigny.schmidt@gmail.com>
2020-06-15 15:59:11 +0200

    Updates

---
+++ precache-manifest.082e94b190652f33ed5771e665d2f138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942cb71f9e039343c330bc37e03f9a8f",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "934dc092845aa368aaa3",
+    "url": "/react-todolist/static/css/main.7a20578a.chunk.css"
+  },
+  {
+    "revision": "f9c3a54b34d8700e2a4f",
+    "url": "/react-todolist/static/js/2.a76daf7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.a76daf7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "934dc092845aa368aaa3",
+    "url": "/react-todolist/static/js/main.2083b52a.chunk.js"
+  },
+  {
+    "revision": "2985a5940d99d7bb198d",
+    "url": "/react-todolist/static/js/runtime-main.c40ba9bd.js"
+  }
+]);
